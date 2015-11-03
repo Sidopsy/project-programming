c44052c2562ba7425e41c14ae146c797202b438b
maansthoernvik <thornvik.mans@gmail.com>
2015-11-03 22:05:49 +0100

    Commented and formatted viewAd
    Commented the code with ordinary comments as well as javadocs.

--- BeeAdopted/src/ViewAd.java
+++ BeeAdopted/src/ViewAd.java
@@ -13,87 +13,112 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * Class for showing an Ad in a separate window. This window contains more information about said Ad as well as an option
+ * to press an Adopt button. The Adopt button should remove the Ad from being listed in the application and prompt the
+ * user to input feedback on the transaction with a 0 to 5 rating and a comment.
+ * 
+ * @author ML
+ */
+
 public class ViewAd {
 	static Stage window;
-	static Scene sc1, sc2;
-	static BorderPane layout1, layout2;
+	static Scene scAd, scAdopt;
+	static BorderPane layoutAd, layoutAdopt;
 	
 	public static void display(String title, Ad ad){
-		//Ad ad = new Ad(title, title, title, title, title, adID, title, title, title);
-		
-		
 		window = new Stage();
+		
+		// This enables the iAdopt image to be shown.
 		StackPane head = new StackPane();
 		Image image = new Image("iAdopt.png");
 		ImageView header = new ImageView();
 		header.setImage(image);
 		head.getChildren().add(header);
 		
-		window.initModality(Modality.APPLICATION_MODAL);
-		window.setTitle(title);
-		window.setMinWidth(250);
+		// Sets Modality and window title.
+		window.initModality(Modality.APPLICATION_MODAL);	// Application modal makes sure that input to the app is only
+		window.setTitle(title);								// entered to the currently open window. This enum needs to be
+		window.setMinWidth(250);							// set before anything is shown in this stage.
 		
-		//Ad view
-		layout1 = new BorderPane();
-		GridPane adView = getAd(ad);
-		layout1.setTop(head);
-		layout1.setCenter(adView);
-		sc1 = new Scene(layout1,600,400);
+		// This is the Ad view of this Stage.
+		layoutAd = new BorderPane();
+		GridPane viewAd = getAd(ad);						// Sends the input Ad into getAd() to be able to show it.
+		layoutAd.setTop(head);								// Show the iAdopt image.
+		layoutAd.setCenter(viewAd);							// Center of the BorderPane will show the Ad.
+		scAd = new Scene(layoutAd,600,400);
 				
-		//Adopt view
-		layout2 = new BorderPane();
-		GridPane adoptView = adopted();
-		layout2.setTop(head);
-		layout2.setCenter(adoptView);
-		sc2 = new Scene(layout2,600,400);
+		// This is the view of the Ad once adopt is pressed.
+		layoutAdopt = new BorderPane();
+		GridPane viewAdopt = adopted();
+		layoutAdopt.setTop(head);							// Show the iAdopt image.
+		layoutAdopt.setCenter(viewAdopt);					// Center of the BorderPane will show the Adopted layout.
+		scAdopt = new Scene(layoutAdopt,600,400);
 		
-		window.setScene(sc1);
+		// Ad scene will be shown first and the App awaits instructions.
+		window.setScene(scAd);
 		window.showAndWait();
 	}
 
+	/**
+	 * This method returns a GridPane containing the information on a specific Ad that was clicked in a previous window.
+	 * A button for adopting said item will be available as well as a close button that will close the entire window.
+	 * 
+	 * @param Ad
+	 * @return GridPane
+	 */
 	
 	private static GridPane getAd(Ad ad) {
-		GridPane grid = new GridPane();
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(0,10,0,10));
 		
+		// Gridpane in which the Ad will be shown.
+		GridPane grid = new GridPane();
+		grid.setHgap(10);										// Horizontal gaps between columns.
+		grid.setVgap(10);										// Vertical gaps between columns.
+		grid.setPadding(new Insets(0,10,0,10));					// Setting the padding around the content.
+		
+		// Labels for displaying information about said Ad.
 		Label name = new Label();
 		grid.add(name,1,0);
-		
 		Label species = new Label();
 		grid.add(species, 2, 0);
 		
-		//String[] adInfo = DatabaseConnection.getAd(adID);
-		
+		// Button for adopting the Ad, this sets the scene to Adopted.
 		Button adoptButton = new Button("Adopt");
 		grid.add(adoptButton, 0,3);
-		adoptButton.setOnAction(e -> window.setScene(sc2));
-		
-		
+		adoptButton.setOnAction(e -> window.setScene(scAdopt));	// If button Adopt is pressed it takes the user to the
+																// Adopted scene specified above.
+		// Closes the window and takes you back to the Ad overview.
 		Button closeButton = new Button("Close the window");
 		grid.add(closeButton, 4, 4);
-		closeButton.setOnAction(e -> window.close());
+		closeButton.setOnAction(e -> window.close());			// Closes the window.
 		
 		return grid;
 	}
+	
+	/**
+	 * This method returns a GridPane containing an appropriate message one should recieve when pressing the Adopt button 
+	 * for a specific Ad. 
+	 * 
+	 * @return GridPane
+	 */
 	
 	private static GridPane adopted() {
-		GridPane grid = new GridPane();
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(0,10,0,10));
 		
-		Label label = new Label("CONGRATULATIONS!!!");
+		// GridPane in which the Adopted view will be shown.
+		GridPane grid = new GridPane();
+		grid.setHgap(10);											// Horizontal gaps between columns.
+		grid.setVgap(10);											// Vertical gaps between columns.
+		grid.setPadding(new Insets(0,10,0,10));						// Setting the padding around the content.
+		
+		// Label containing information to the shown to the user when pressing Adopt.
+		Label label = new Label("CONGRATULATIONS MOTHERTRUCKER!");	// Friendly message shown when Adopt is pressed.
 		grid.add(label, 0, 0);
 		
+		// Same function as in other scene. Closes the window.
 		Button closeButton = new Button("Close the window");
 		grid.add(closeButton, 4, 4);
-		closeButton.setOnAction(e -> window.close());
+		closeButton.setOnAction(e -> window.close());				// Closes the window.
 		
-		return grid;
-		
+		return grid;		
 	}
-
-	
 }
